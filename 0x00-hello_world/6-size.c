02eede59abd1ddf71cdd9e4ad84fdee364fcd6d3
David Udo <udodavid46.ud@gmail.com>
2022-09-08 06:44:41 -0700

    Solved 6-size.c task

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,4 +12,5 @@ int main(void)
 	printf("Size of a long int: %i byte(s)\n", sizeOf(long int));
 	printf("Size of a long long int: %i byte(s)\n", sizeOf(long long int));
 	printf("Size of a float: %i byte(s)\n", sizeOf(float));
+	return (0);
 }
