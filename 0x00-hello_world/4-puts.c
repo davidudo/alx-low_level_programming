006bde3d9302a893463fb65ae39851e6b18eac08
David Udo <udodavid46.ud@gmail.com>
2022-09-08 06:21:47 -0700

    Solved 4-puts.c task

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- * main block prints the below string:
- * Programming is like building a multilingual puzzle
+ * main - main block
+ * Return: 0
  */
 
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }
