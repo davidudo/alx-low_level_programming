b704180f0d4aa98069d0605c03db758b23b6db03
David Udo <udodavid46.ud@gmail.com>
2022-09-13 20:53:15 +0100

    Solved 6-abs.c task

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _abs - Compute the absolute value of an integer
+ *
+ * @num: type int
+ * 
+ * Return: absolute value of @n
+ */
+int _abs(int num)
+{
+	if (n < 0)
+	{
+		return (num * -1);
+	}
+	else
+	{
+		return (num);
+	}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -3,4 +3,5 @@ void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int ch);
 int _isalpha(int ch);
-int print_sign(int n);
+int print_sign(int num);
+int _abs(int);
