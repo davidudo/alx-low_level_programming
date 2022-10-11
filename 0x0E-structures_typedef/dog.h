70902b6e6e7b9fc1672ad06ab699f2d31e18994d
davidudo <udodavid46.ud@gmail.com>
2022-10-11 03:50:52 +0100

    Solved dog.h task

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - contains details about a dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: name of the dog's owner
+ *
+ * Description: holds all details pertaining to a dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */
