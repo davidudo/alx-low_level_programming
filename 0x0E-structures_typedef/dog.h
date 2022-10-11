a9ab509411547e821128952c7ce85485e72d72a5
davidudo <udodavid46.ud@gmail.com>
2022-10-11 04:47:52 +0100

    Solved dog_t task

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -9,13 +9,19 @@
  *
  * Description: holds all details pertaining to a dog
  */
-
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+
+/**
+ * dog_t - Typedef for struct dog
+ *
+ * Description: New data structure type of struct dog
+ */
+typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
