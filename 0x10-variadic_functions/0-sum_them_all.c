92fe7256831370ba2e0f8db54bdaf3570385f4ca
davidudo <udodavid46.ud@gmail.com>
2022-10-14 01:57:06 +0100

    Solved 0-sum_them_all task

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -10,14 +10,13 @@
 int sum_them_all(const unsigned int n, ...)
 {
 	va_list argList;
-	unsigned int i, sum;
+	unsigned int i, sum = 0;
 
 	if (n == 0)
 		return(0);
 
 	va_start(argList, n);
 
-	sum = 0;
 	for (i = 0; i < n; i++)
 		sum += va_arg(argList, int);
 
