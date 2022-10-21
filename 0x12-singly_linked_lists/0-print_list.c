04a8ef6bb1088d95e18166b2f38ca8ca375b50e5
davidudo <udodavid46.ud@gmail.com>
2022-10-21 03:29:37 +0100

    Solved 0-print_list.c task

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,37 +1,25 @@
 #include "lists.h"
 
-int _strlen(char *s);
-
+/**
+ * print_list - returns the number of nodes
+ * @h: struct list_s
+ *
+ * Returns: number of nodes
+ */
 size_t print_list(const list_t *h)
 {
 	size_t count;
-	int str_length;
 
 	count = 0;
 	while (h != NULL)
 	{
 		if (h->str == NULL)
-			printf("[0] (nil)");
-
-		str_length = _strlen(h->str);
-		printf("[%d] %s\n", str_length, h->str);
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
 		count += 1;
 	}
 
-	return (count);
-}
-
-/**
- * _strlen - returns the length of a string.
- * @s: input stringeturn.
- * Return: length of a string.
- */
-int _strlen(char *s)
-{
-	int count = 0;
-
-	while (*(s + count) != '\0')
-		count++;
 	return (count);
 }
