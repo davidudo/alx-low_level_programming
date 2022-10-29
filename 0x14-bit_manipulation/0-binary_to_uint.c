be11b6f145982c009db5edd12751f957361489e5
davidudo <udodavid46.ud@gmail.com>
2022-10-29 01:40:17 +0100

    Solved 0-binary_to_uint.c task

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * binary_to_unit - converts a binary to an unsigned int
+ * binary_to_uint - converts a binary to an unsigned int
  * @b: binary number
  *
  * Return: converted number or 0
